f7fcd1910fb0d98494d231c0a4c040294b047af8
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-12-20 05:45:33 -0800

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A class Square to define a square"""
+
+
+class Square:
+    """A class Square
+
+    Attributes:
+       size (int): size of the square
+    """
+    def __init__(self, size):
+        """Initialize the class Square
+
+        Args:
+            size: size of the square
+        """
+        self.__size = size
